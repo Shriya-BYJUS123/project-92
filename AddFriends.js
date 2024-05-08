d10caf43e57947849b52cb15189b7dcfdb8e4eed
Shriya-BYJUS123 <131769367+Shriya-BYJUS123@users.noreply.github.com>
2024-05-08 20:21:33 +1000

    Add files via upload

---
+++ AddFriends.js
@@ -0,0 +1,10 @@
+import React from "react"
+import {Text, View, StyleSheet,TouchableOpacity} from "react-native"
+
+export default class AddFriends extends React.Component{
+    render(){
+        return(
+            <Text>Add Friends</Text>
+        )
+    }
+}
---
+++ Request.js
@@ -0,0 +1,10 @@
+import React from "react"
+import {Text, View, StyleSheet,TouchableOpacity} from "react-native"
+
+export default class Request extends React.Component{
+    render(){
+        return(
+            <Text>Request</Text>
+        )
+    }
+}
